68fd43678291c6f5ce150ccea15b44840eea6ffd
Parkna98 <pkpphn@naver.com>
2023-12-15 16:15:41 +0900

    111

--- JSPPetestivaliProject/src/main/java/com/sist/model/MainModel.java
+++ JSPPetestivaliProject/src/main/java/com/sist/model/MainModel.java
@@ -4,11 +4,27 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.sist.controller.RequestMapping;
+import java.util.*;
+import com.sist.dao.*;
+import com.sist.vo.*;
 
 public class MainModel {
 	@RequestMapping("main/main.do")
 	public String main_main(HttpServletRequest request, HttpServletResponse response)
 	{
+		
+		String fd=request.getParameter("fd");
+		if(fd==null) {
+			fd="가평";
+		}
+		StayDAO dao=StayDAO.newInstance();
+		List<StayVO> sList=dao.stayAddressListData(fd);
+		ProductDAO pdao=ProductDAO.newInstance();
+		List<ProductVO>plist =pdao.homeProduct();
+		
+//		request.setAttribute("plist", plist);
+		request.setAttribute("fd", fd);
+		request.setAttribute("sList", sList);
 		request.setAttribute("main_jsp", "../main/home.jsp");
 		return "../main/main.jsp";
 	}
